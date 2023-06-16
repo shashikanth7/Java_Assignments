c16a1e6b16e0f71900781d5f4891dc68d4495f3f
shashikanth <shashikanth12252gmail.com>
2023-06-16 16:05:10 +0530

    first

---
+++ 15-06-2023 maven assigment/Task 1/Task 1/User.java
@@ -0,0 +1,31 @@
+package com.dal.setter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class User {
+	@Value("shashi")
+	private String userName;
+	@Autowired
+	private Car car;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Car getCar() {
+		return car;
+	}
+	public void setCar(Car car) {
+		this.car = car;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", car=" + car + "]";
+	}
+
+}
